872d3112210ae390398d92e200705506f5000499
Cristoph <Master666bitbucket>
2018-12-06 00:08:35 -0300

    update model

--- app_backend/models.py
+++ app_backend/models.py
@@ -5,11 +5,10 @@ from django.db import models
 class Farm(models.Model):
     uuid = models.CharField(max_length=10, unique=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    #description = models.TextField(blank=True, null=True)
-    sensor_1 = models.PositiveIntegerField(default=0)
-    sensor_2 = models.PositiveIntegerField(default=0)
-    sensor_3 = models.PositiveIntegerField(default=0)
-    sensor_4 = models.PositiveIntegerField(default=0)
+    description = models.TextField(blank=True, null=True)
+    sensor_temp_air = models.PositiveIntegerField(default=0)
+    sensor_temp_water = models.PositiveIntegerField(default=0)
+    sensor_light = models.PositiveIntegerField(default=0)
     last_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -18,10 +17,9 @@ class Farm(models.Model):
 
 class History(models.Model):
     farm = models.ForeignKey(Farm, on_delete=models.CASCADE, related_name='history')
-    sensor_1 = models.PositiveIntegerField(default=0)
-    sensor_2 = models.PositiveIntegerField(default=0)
-    sensor_3 = models.PositiveIntegerField(default=0)
-    sensor_4 = models.PositiveIntegerField(default=0)
+    sensor_temp_air = models.PositiveIntegerField(default=0)
+    sensor_temp_water = models.PositiveIntegerField(default=0)
+    sensor_light = models.PositiveIntegerField(default=0)
     date = models.DateTimeField()
 
     def __str__(self):
