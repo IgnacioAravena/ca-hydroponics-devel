803ac85b9f498180215c61c8036b7366ecaa7e64
Cristoph <cristopher.alejandro@gmail.com>
2018-12-14 01:28:44 -0300

    avances

--- app_backend/models.py
+++ app_backend/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Seed(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='static/img/farm/')
+    image = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
@@ -13,7 +13,6 @@ class Seed(models.Model):
 class Farm(models.Model):
     uuid = models.CharField(max_length=10, unique=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    #name = models.CharField(max_length=120)
     seed = models.ForeignKey(Seed, null=True, on_delete=models.PROTECT)
     sensor_temp_air = models.PositiveIntegerField(default=0)
     sensor_temp_water = models.PositiveIntegerField(default=0)
